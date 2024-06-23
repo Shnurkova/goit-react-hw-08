edce87f4558df1c70e097b88fb63b47feb17162c
Shnurkova <Annshnur@gmail.com>
2024-06-23 12:01:40 +0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import AppBar from './components/AppBar/AppBar.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, lazy, Suspense } from 'react';
+import { selectIsRefreshing } from './redux/auth/selectors';
+import { refreshUser } from './redux/auth/operations';
+import { Route, Routes } from 'react-router-dom';
+import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute.jsx';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute.jsx';
+import { Toaster } from 'react-hot-toast';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+const RegistrationPage = lazy(() =>
+  import('./pages/RegistrationPage/RegistrationPage.jsx')
+);
+const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage.jsx'));
+const ContactsPage = lazy(() =>
+  import('./pages/ContactsPage/ContactsPage.jsx')
+);
+const NotFoundPage = lazy(() =>
+  import('./pages/NotFoundPage/NotFoundPage.jsx')
+);
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { isRefreshing } = useSelector(selectIsRefreshing);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <div
+      style={{
+        margin: '0 auto',
+
+        maxWidth: 1280,
+        textAlign: 'center',
+      }}
+    >
+      <AppBar />
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<RegistrationPage />}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<LoginPage />}
+              />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+            }
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+      <Toaster position="top-center" toastOptions={{ duration: 6000 }} />
+    </div>
+  );
+};
+
+export default App;
